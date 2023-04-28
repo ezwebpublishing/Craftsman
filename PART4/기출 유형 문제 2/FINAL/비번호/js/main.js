31fe03bc59ace39419998c5ede56c3f7f18e67ab
rock's ezweb <109511441+ezwebpublishing@users.noreply.github.com>
2023-04-29 08:51:45 +0900

    indentation change

--- PART4/기출 유형 문제 2/FINAL/비번호/js/main.js
+++ PART4/기출 유형 문제 2/FINAL/비번호/js/main.js
@@ -3,11 +3,11 @@ let subMenu = mainMenu.find('ul');
 let header = $('header');
 
 mainMenu.mouseover(function () {
-    header.addClass('active');
-    subMenu.stop().slideDown();
+  header.addClass('active');
+  subMenu.stop().slideDown();
 }).mouseout(function () {
-    header.removeClass('active');
-    subMenu.stop().slideUp();
+  header.removeClass('active');
+  subMenu.stop().slideUp();
 });
 
 //슬라이드
@@ -17,14 +17,14 @@ let slideCount = slide.length;
 let currentIdx = 0;
 
 function autoSlide() {
-    setInterval(function () {
-        //3초마다 반복수행 할 구문 시작        
-        nextIdx = (currentIdx + 1) % slideCount;
-        slideContainer.animate({
-            marginLeft: -1200 * nextIdx + 'px'
-        });
-        currentIdx = nextIdx;
-    }, 3000)
+  setInterval(function () {
+    //3초마다 반복수행 할 구문 시작        
+    nextIdx = (currentIdx + 1) % slideCount;
+    slideContainer.animate({
+      marginLeft: -1200 * nextIdx + 'px'
+    });
+    currentIdx = nextIdx;
+  }, 3000)
 }
 autoSlide();
 
@@ -34,10 +34,10 @@ let popup = $('#popup');
 let popupCloseBtn = popup.find('.close');
 
 popupLink.click(function (e) {
-    e.preventDefault();
-    popup.addClass('active');
+  e.preventDefault();
+  popup.addClass('active');
 });
 
 popupCloseBtn.click(function () {
-    popup.removeClass('active');
+  popup.removeClass('active');
 });
