76b985a5f47a57e5bbc329d551eeab035ccb14ea
rock's ezweb <109511441+ezwebpublishing@users.noreply.github.com>
2023-05-03 22:46:32 +0900

    Add files via upload

--- PART4/기출 유형 문제 6(D1형)/FINAL/script/main.js
+++ PART4/기출 유형 문제 6(D1형)/FINAL/script/main.js
@@ -1,8 +1,8 @@
-let mainMenu = $('.main_menu>li');
+let mainMenu = $('.main-menu>li');
 mainMenu.mouseover(function () {
-    $(this).find('ul').stop().slideDown();
+  $(this).find('ul').stop().slideDown();
 }).mouseout(function () {
-    $(this).find('ul').stop().slideUp();
+  $(this).find('ul').stop().slideUp();
 });
 
 //슬라이드
@@ -12,14 +12,14 @@ let slideCount = slide.length;
 let currentIdx = 0;
 
 function autoSlide() {
-    setInterval(function () {
-        //3초마다 반복수행 할 구문 시작   
-        let nextIdx = (currentIdx + 1) % slideCount;
-        slideContainer.animate({
-            marginTop: -400 * nextIdx + 'px'
-        });
-        currentIdx = nextIdx;
-    }, 3000)
+  setInterval(function () {
+    //3초마다 반복수행 할 구문 시작   
+    let nextIdx = (currentIdx + 1) % slideCount;
+    slideContainer.animate({
+      marginTop: -400 * nextIdx + 'px'
+    });
+    currentIdx = nextIdx;
+  }, 3000)
 }
 autoSlide();
 
@@ -28,12 +28,12 @@ let tabMenu = $('.tabmenu li');
 let tabContent = $('.tabcontent > div');
 
 tabMenu.click(function (e) {
-    e.preventDefault();
-    tabMenu.removeClass('active');
-    $(this).addClass('active');
-    let target = $(this).find('a').attr('href');
-    tabContent.removeClass('active');
-    $(target).addClass('active');
+  e.preventDefault();
+  tabMenu.removeClass('active');
+  $(this).addClass('active');
+  let target = $(this).find('a').attr('href');
+  tabContent.removeClass('active');
+  $(target).addClass('active');
 });
 //팝업
 let popupLink = $('#notice li:first');
@@ -41,10 +41,10 @@ let popup = $('#popup');
 let popupCloseBtn = popup.find('.close');
 
 popupLink.click(function (e) {
-    e.preventDefault();
-    popup.addClass('active');
+  e.preventDefault();
+  popup.addClass('active');
 });
 
 popupCloseBtn.click(function () {
-    popup.removeClass('active');
+  popup.removeClass('active');
 });
