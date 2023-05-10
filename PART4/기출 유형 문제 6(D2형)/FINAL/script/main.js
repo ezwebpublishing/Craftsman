cf69c5df9662c86754f3ab67c92333331684228c
rock's ezweb <109511441+ezwebpublishing@users.noreply.github.com>
2023-05-10 16:58:42 +0900

    indentation modified tab removed

--- PART4/기출 유형 문제 6(D2형)/FINAL/script/main.js
+++ PART4/기출 유형 문제 6(D2형)/FINAL/script/main.js
@@ -1,8 +1,9 @@
+//메뉴
 let mainMenu = $('.main-menu>li');
 mainMenu.mouseover(function () {
-    $(this).find('ul').stop().slideDown();
+  $(this).find('ul').stop().slideDown();
 }).mouseout(function () {
-    $(this).find('ul').stop().slideUp();
+  $(this).find('ul').stop().slideUp();
 });
 
 //슬라이드
@@ -12,29 +13,16 @@ let slideCount = slide.length;
 let currentIdx = 0;
 
 function autoSlide() {
-    setInterval(function () {
-        //3초마다 반복수행 할 구문 시작   
-        let nextIdx = (currentIdx + 1) % slideCount;
-        slideContainer.animate({
-            left: -100 * nextIdx + '%'
-        });
-        currentIdx = nextIdx;
-    }, 3000)
+  setInterval(function () {
+    //3초마다 반복수행 할 구문 시작   
+    let nextIdx = (currentIdx + 1) % slideCount;
+    slideContainer.animate({
+      left: -100 * nextIdx + '%'
+    });
+    currentIdx = nextIdx;
+  }, 3000)
 }
 autoSlide();
-
-//탭
-let tabMenu = $('.tabmenu li');
-let tabContent = $('.tabcontent > div');
-
-tabMenu.click(function (e) {
-    e.preventDefault();
-    tabMenu.removeClass('active');
-    $(this).addClass('active');
-    let target = $(this).find('a').attr('href');
-    tabContent.removeClass('active');
-    $(target).addClass('active');
-});
 //팝업
 let popupLink = $('#notice li:first');
 let popup = $('#popup');
