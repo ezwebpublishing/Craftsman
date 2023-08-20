c266390d24cfd411bd459cdf39dea877362eb552
rock's ezweb <alikerock@gmail.com>
2023-08-20 21:39:42 +0900

    part4 유형2 update

--- PART4/기출 유형 문제 2/FINAL/비번호/script/main.js
+++ PART4/기출 유형 문제 2/FINAL/비번호/script/main.js
@@ -1,39 +1,33 @@
-let mainMenu = $('.main_menu>li');
+let mainMenu = $('nav > ul > li');
+let subMenu = mainMenu.find('ul');
+let header = $('header');
 mainMenu.mouseover(function () {
-  $(this).find('ul').stop().slideDown();
+  header.addClass('active');
+  subMenu.stop().slideDown();
 }).mouseout(function () {
-  $(this).find('ul').stop().slideUp();
+  header.removeClass('active');
+  subMenu.stop().slideUp();
 });
 
 //슬라이드
-let slideContainer = $('.slide_container');
+let slideContainer = $('.container');
 let slide = slideContainer.find('.slide');
 let slideCount = slide.length;
 let currentIdx = 0;
-slide.eq(currentIdx).fadeIn();
-setInterval(showNextSlide, 3000);
 
-function showNextSlide() {
-  let nextIdx = (currentIdx + 1) % slideCount;
-  slide.eq(currentIdx).fadeOut();
-  slide.eq(nextIdx).fadeIn();
-  currentIdx = nextIdx;
+function autoSlide() {
+  setInterval(function () {
+    //3초마다 반복수행할 구문 시작
+    nextIdx = (currentIdx + 1) % slideCount;
+    slideContainer.animate({
+      marginLeft: -1200 * nextIdx + 'px'
+    });
+    currentIdx = nextIdx;
+  }, 3000)
 }
+autoSlide();
 
-let tabMenu = $('.tabmenu li');
-let tabContent = $('.tabcontent > div');
-tabMenu.click(function (e) {
-  e.preventDefault();
-  tabMenu.removeClass('active');
-  $(this).addClass('active');
-
-  let target = $(this).find('a').attr('href');
-  tabContent.removeClass('active');
-  $(target).addClass('active');
-});
-
-//팝업
-let popupLink = $('#notice li:first');
+let popupLink = $('.notice li:first');
 let popup = $('#popup');
 let popupCloseBtn = popup.find('.close');
 popupLink.click(function (e) {
