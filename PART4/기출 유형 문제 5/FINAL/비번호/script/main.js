cf08bb1cf5a11edeeda202c37aa61c31703d2495
rock's ezweb <alikerock@gmail.com>
2023-08-20 22:37:09 +0900

    part4 기출문제5 update

--- PART4/기출 유형 문제 5/FINAL/비번호/script/main.js
+++ PART4/기출 유형 문제 5/FINAL/비번호/script/main.js
@@ -1,12 +1,10 @@
 let mainMenu = $('.main_menu > li');
-
 mainMenu.mouseover(function () {
   $(this).find('ul').stop().slideDown();
 }).mouseout(function () {
   $(this).find('ul').stop().slideUp();
 });
 
-//슬라이드
 let slideContainer = $('.container');
 let slide = slideContainer.find('.slide');
 let slideCount = slide.length;
@@ -14,7 +12,7 @@ let currentIdx = 0;
 
 function autoSlide() {
   setInterval(function () {
-    //3초마다 반복수행 할 구문 시작   
+    //3초마다 반복수행할 구문 시작
     let nextIdx = (currentIdx + 1) % slideCount;
     slideContainer.animate({
       marginTop: -300 * nextIdx + 'px'
@@ -24,24 +22,19 @@ function autoSlide() {
 }
 autoSlide();
 
-//팝업
 let popupLink = $('#notice li:first');
 let popup = $('#popup');
 let popupCloseBtn = popup.find('.close');
-
 popupLink.click(function (e) {
   e.preventDefault();
   popup.addClass('active');
 });
-
 popupCloseBtn.click(function () {
   popup.removeClass('active');
 });
 
-//탭
 let tabMenu = $('.tabmenu li');
 let tabContent = $('.tabcontent > div');
-
 tabMenu.click(function (e) {
   e.preventDefault();
   tabMenu.removeClass('active');
